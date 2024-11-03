d0737e76904a78e120ddd6a318707f22773a2f4d
Jeyan B <jeyantriton@gmail.com>
2024-11-03 20:24:40 +0530

    cors origin

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ app.use(cors());
 app.use(function (req, res, next){
     // res.header("Access-Control-Allow-Origin", "http://localhost:3000");
     // res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With Content-Type, Accept");
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Origin", "https://vocal-truffle-078b46.netlify.app");
+    // res.header("Access-Control-Allow-Origin", "*");
    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
             next();
 })
